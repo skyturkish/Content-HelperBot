f853f6ed7e007523ab683431b4ada5c2468a34f3
sky <gokturk.acr2002@gmail.com>
2022-12-19 17:27:26 +0300

    Handle selectMenu The user can select videos according to the video index

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,23 +3,30 @@ const { Events } = require('discord.js')
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return
-
-        const command = interaction.client.commands.get(interaction.commandName)
-
-        if (!command) {
-            console.error(
-                `No command matching ${interaction.commandName} was found.`
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(
+                interaction.commandName
             )
-            return
-        }
+            if (!command) {
+                console.error(
+                    `No command matching ${interaction.commandName} was found.`
+                )
+                return
+            }
+            try {
+                await command.execute(interaction)
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`)
+                console.error(error)
+            }
+        } else if (interaction.isStringSelectMenu()) {
+            if (interaction.customId === 'videos') {
+                const secondInformation = interaction.values[0]
 
-        try {
-            console.log('komut calistirildi ???')
-            await command.execute(interaction)
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`)
-            console.error(error)
+                interaction.client.commands
+                    .get('youtubeurl')
+                    .execute(interaction, secondInformation)
+            }
         }
     },
 }
