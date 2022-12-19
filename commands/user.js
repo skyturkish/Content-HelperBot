eb89bfc16ad7e2b234c18190b76968151dd78f85
sky <gokturk.acr2002@gmail.com>
2022-12-19 17:26:17 +0300

    brings up videos related to the input word The user can see the videos related to the word user entered Can select video from the shown item menu User can see the video related to the word

--- commands/user.js
+++ commands/user.js
@@ -5,10 +5,12 @@ module.exports = {
         .setName('user')
         .setDescription('Provides information about the user.'),
     async execute(interaction) {
-        // interaction.user is the object representing the User who ran the command
-        // interaction.member is the GuildMember object, which represents the user in the specific guild
-        await interaction.reply(
-            `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
-        )
+        const list = ['Option 1', 'Option 2', 'Option 3']
+        let message =
+            'Please select an option from the list by its index number:\n'
+        list.forEach((option, index) => {
+            message += `${index + 1}. ${option}\n`
+        })
+        await interaction.reply(message)
     },
 }
---
+++ commands/videos.js
@@ -0,0 +1,121 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ActionRowBuilder,
+    StringSelectMenuBuilder,
+} = require('discord.js')
+
+var json = require('../data/videos.json')
+
+const wait = require('node:timers/promises').setTimeout
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('videos')
+        .setDescription('Get information about the nimble')
+        .addStringOption((option) =>
+            option
+                .setName('content')
+                .setDescription('content aramanı sağlayacak kelimeyi yaz')
+                .setMinLength(3)
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const inputValue = interaction.options
+            .getString('content')
+            .toLowerCase()
+
+        const videos = json['videos']
+
+        const videoInformations = []
+
+        for (let i = 0; i < videos.length; i++) {
+            const video = videos[i]
+
+            const timeStamps = video['timeStamps']
+
+            const createdAt = video['createdAt']
+
+            for (let index = 0; index < timeStamps.length; index++) {
+                const timeStamp = timeStamps[index]
+
+                const text = timeStamp['text'].toLowerCase()
+
+                if (text.includes(inputValue)) {
+                    videoInformations.push({
+                        videoIndex: i,
+                        createdAt: createdAt,
+                        videoTimeStampText: text,
+                        timeStampIndex: index,
+                    })
+                    break
+                }
+            }
+        }
+
+        if (videoInformations.length == 0) {
+            await interaction.reply({
+                content: 'there is no video about ' + inputValue,
+                ephemeral: true,
+            })
+            return
+        }
+
+        const row = new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId('videos')
+                .setPlaceholder('Nothing selected')
+                .addOptions(
+                    videoInformations
+                        .slice(0, 25)
+                        .map(function (currentvalue, index) {
+                            return {
+                                label: (index + 1).toString(),
+                                // value can carry only String
+                                value:
+                                    videoInformations[index][
+                                        'videoIndex'
+                                    ].toString() +
+                                    ':' +
+                                    videoInformations[index][
+                                        'timeStampIndex'
+                                    ].toString(),
+                            }
+                        })
+                )
+        )
+
+        let videoTimeStamps = ''
+
+        videoInformations.slice(0, 25).forEach((currentvalue, index) => {
+            const videoTimeStampText = currentvalue['videoTimeStampText']
+            const videoTimeStampCreatedAt = currentvalue['createdAt']
+
+            videoTimeStamps =
+                `${videoTimeStamps} ${index + 1} - ${videoTimeStampText} ` +
+                videoTimeStampCreatedAt +
+                '\n'
+        })
+
+        // change url with website has all video can be searched
+        const embed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle('Videos')
+            .setURL('https://discord.js.org/')
+            .setDescription(videoTimeStamps)
+
+        interaction
+            .reply({
+                embeds: [embed],
+                ephemeral: true,
+                components: [row],
+            })
+            .then(() => console.log('Reply sent.'))
+            .catch(console.error)
+
+        await wait(60000)
+
+        interaction.deleteReply().then(console.log).catch(console.error)
+    },
+}
